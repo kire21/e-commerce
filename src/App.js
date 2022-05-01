671803c800e62c8ab764bfb56fb75867ce2e0b09
kire21 <kire.zdraveski@gmail.com>
2022-05-01 12:11:55 +0200

    add Outlet from react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/Home";
 
+const Shop = () => {
+  return <div>I am the shop page</div>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />}>
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };

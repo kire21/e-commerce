90f4d0853fc6b69f43c8ecce5ef852f6d450461d
kire21 <kire.zdraveski@gmail.com>
2022-05-25 19:37:41 +0200

    implement context for products

--- src/routes/shop/Shop.js
+++ src/routes/shop/Shop.js
@@ -1,9 +1,12 @@
-import SHOP_DATA from "../../shop-data.json";
+import { useContext } from "react";
+
+import { ProductsContext } from "../../contexts/products.context";
 
 const Shop = () => {
+  const { products } = useContext(ProductsContext);
   return (
     <div>
-      {SHOP_DATA.map((product) => {
+      {products.map((product) => {
         return (
           <div key={product.id}>
             <h1>{product.name}</h1>
